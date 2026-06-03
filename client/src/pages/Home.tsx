4292890265956ff2463d9bd2142131f5b170096a
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2026-06-03 18:10:27 +0530

    Added CTABanner and Pricing components to enhance homepage functionality

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,15 @@
+import CTABanner from "../components/ctaBanner";
 import Features from "../components/features";
 import Hero from "../components/hero";
+import Pricing from "../components/pricing";
 
 const Home = () => {
   return (
     <div className="bg-page">
       <Hero />
       <Features />
+      <Pricing />
+      <CTABanner />
     </div>
   );
 };
