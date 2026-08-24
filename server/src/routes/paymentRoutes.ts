6832f32d66195dca73f0fa2809a2b5b3f69d3097
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2026-08-24 17:13:43 +0530

    Add payment processing routes for checkout and verification

--- server/src/routes/paymentRoutes.ts
+++ server/src/routes/paymentRoutes.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import { isAuth } from "../middleware/isAuth.js";
+import { checkout, paymentVerification } from "../controllers/payment.js";
 
 const router = express.Router();
+
+router.post("/checkout", isAuth, checkout);
+router.post("/verify", isAuth, paymentVerification);
 
 export default router;
