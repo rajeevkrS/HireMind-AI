4728e41da074119cb4c5ea87d6c6327f1c3629c6
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2026-08-25 17:28:26 +0530

    Update server configuration and add Vercel deployment files

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-dotenv.config();
+dotenv.config({ quiet: true });
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
@@ -13,12 +13,19 @@ export const instance = new Razorpay({
 });
 await connectDB();
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/user", userRoutes);
 app.use("/api/ai", aiRoutes);
 app.use("/api/payment", paymentRoutes);
+app.get("/", (req, res) => {
+    res.json({ message: "HireMind AI Backend Running" });
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
