d26ae072164e7bac1a1568d9c65748e34946dcb6
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2026-08-26 16:54:09 +0530

    refactor: simplify dotenv configuration and enable CORS for all origins

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-dotenv.config({ quiet: true });
+dotenv.config();
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
@@ -13,11 +13,7 @@ export const instance = new Razorpay({
 });
 await connectDB();
 const app = express();
-// app.use(cors());
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/user", userRoutes);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-dotenv.config({ quiet: true });
+dotenv.config();
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
@@ -17,15 +17,7 @@ await connectDB();
 
 const app = express();
 
-// app.use(cors());
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
-);
-
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
